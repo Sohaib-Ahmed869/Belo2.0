7e05402ab228aabad6d24ed391cbee3e6ae76a53
Sohaib-Ahmed869 <i211114@nu.edu.pk>
2024-06-28 11:37:15 +0500

    Dashboard layout

--- src/Layout.js
+++ src/Layout.js
@@ -6,13 +6,16 @@ import Signup from "./Signup/page";
 import ForgetPassword from "./Login/forgetpass";
 import OTP from "./Login/otpverificiation";
 
+import DashboardLayout from "./AdminDashboard/DashboardLayout";
+
 const Layout = () => {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/forgetpassword" element={<ForgetPassword />} />
-        <Route path="/otp" element={<OTP />} />
+      <Route path="/otp" element={<OTP />} />
+      <Route path="/admindashboard" element={<DashboardLayout />} />
     </Routes>
   );
 };
