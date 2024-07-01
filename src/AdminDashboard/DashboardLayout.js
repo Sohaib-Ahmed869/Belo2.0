d4528bd403ce0624afb3ae6a342287624b916477
Musa <musaharoon.2003@gmail.com>
2024-07-01 16:24:44 +0500

    Sidebar toggle

--- src/AdminDashboard/DashboardLayout.js
+++ src/AdminDashboard/DashboardLayout.js
@@ -12,9 +12,33 @@ import SubBilling from "./HelpCenter/subBilling";
 import TermsAccess from "./HelpCenter/termsAccess";
 import Integration from "./HelpCenter/integration";
 import TroubleShooting from "./HelpCenter/troubleshooting";
+import { set } from "date-fns";
 
 const DashboardLayout = () => {
   const [activeOption, setActiveOption] = useState("Dashboard");
+  const [showSettingsSuboptions, setShowSettingsSuboptions] = useState(false);
+  const [showHelpCenterSuboptions, setShowHelpCenterSuboptions] =
+    useState(false);
+
+  const handleOptionClick = (option) => {
+    setActiveOption(option);
+    // Close suboptions if another main option is clicked
+    if (option !== "Settings") setShowSettingsSuboptions(false);
+    if (option !== "HelpCenter") setShowHelpCenterSuboptions(false);
+  };
+
+  const toggleSettingsSuboptions = () => {
+    setShowSettingsSuboptions(!showSettingsSuboptions);
+    // Ensure Help Center suboptions are closed when toggling Settings
+    setShowHelpCenterSuboptions(false);
+  };
+
+  const toggleHelpCenterSuboptions = () => {
+    setShowHelpCenterSuboptions(!showHelpCenterSuboptions);
+    // Ensure Settings suboptions are closed when toggling Help Center
+    setShowSettingsSuboptions(false);
+  };
+
   return (
     <div className="flex flex-row w-full">
       <div className="w-1/6 bg-white border-r border-black">
@@ -24,7 +48,11 @@ const DashboardLayout = () => {
             className={`flex items-center gap-2 w-full hover:bg-gray-300 p-4 cursor-pointer hover:font-semibold pl-4 ${
               activeOption === "Dashboard" ? "bg-gray-300" : ""
             }`}
-            onClick={() => setActiveOption("Dashboard")}
+            onClick={() => {
+              setActiveOption("Dashboard");
+              setShowSettingsSuboptions(false);
+              setShowHelpCenterSuboptions(false);
+            }}
           >
             <img src={dashboard} alt="dashboard" className="w-4 mr-2" />
             <p>Dashboard</p>
@@ -33,7 +61,11 @@ const DashboardLayout = () => {
             className={`flex items-center gap-2 w-full hover:bg-gray-300 p-4 cursor-pointer hover:font-semibold pl-4  ${
               activeOption === "Candidates" ? "bg-gray-300" : ""
             }`}
-            onClick={() => setActiveOption("Candidates")}
+            onClick={() => {
+              setActiveOption("Candidates");
+              setShowSettingsSuboptions(false);
+              setShowHelpCenterSuboptions(false);
+            }}
           >
             <img src={dashboard} alt="dashboard" className="w-4 mr-2" />
             <p className="">Candidates</p>
@@ -42,7 +74,11 @@ const DashboardLayout = () => {
             className={`flex items-center gap-2 w-full hover:bg-gray-300 p-4 cursor-pointer hover:font-semibold pl-4 ${
               activeOption === "Roles" ? "bg-gray-300" : ""
             }`}
-            onClick={() => setActiveOption("Roles")}
+            onClick={() => {
+              setActiveOption("Roles");
+              setShowSettingsSuboptions(false);
+              setShowHelpCenterSuboptions(false);
+            }}
           >
             <img src={dashboard} alt="dashboard" className="w-4 mr-2" />
             <p className="">Roles</p>
@@ -51,7 +87,11 @@ const DashboardLayout = () => {
             className={`flex items-center gap-2 w-full hover:bg-gray-300 p-4 cursor-pointer hover:font-semibold pl-4 ${
               activeOption === "Team" ? "bg-gray-300" : ""
             }`}
-            onClick={() => setActiveOption("Team")}
+            onClick={() => {
+              setActiveOption("Team");
+              setShowSettingsSuboptions(false);
+              setShowHelpCenterSuboptions(false);
+            }}
           >
             <img src={dashboard} alt="dashboard" className="w-4 mr-2" />
             <p className="">Team</p>
@@ -60,110 +100,140 @@ const DashboardLayout = () => {
             className={`flex items-center gap-2 w-full hover:bg-gray-300 p-4 cursor-pointer hover:font-semibold pl-4 ${
               activeOption === "Schedule" ? "bg-gray-300" : ""
             }`}
-            onClick={() => setActiveOption("Schedule")}
+            onClick={() => {
+              setActiveOption("Schedule");
+              setShowSettingsSuboptions(false);
+              setShowHelpCenterSuboptions(false);
+            }}
           >
             <img src={dashboard} alt="dashboard" className="w-4 mr-2" />
             <p className="">Schedule</p>
           </div>
           <div
             className={`flex items-center gap-2 w-full hover:bg-gray-300 p-4 cursor-pointer hover:font-semibold pl-4 ${
-              activeOption === "Settings" ? "bg-gray-300" : ""
+              ["Profile", "Billing", "Email"].includes(activeOption)
+                ? "bg-gray-200"
+                : ""
             }`}
-            onClick={() => setActiveOption("Settings")}
+            onClick={() => {
+              setActiveOption("Profile");
+              toggleSettingsSuboptions();
+            }}
           >
             <img src={dashboard} alt="dashboard" className="w-4 mr-2" />
             <p className="">Settings</p>
           </div>
-          <div
-            className={`flex items-center gap-2 pl-10 w-full hover:bg-gray-300 p-4 cursor-pointer hover:font-semibold ${
-              activeOption === "Profile" ? "bg-gray-300" : ""
-            }`}
-            onClick={() => setActiveOption("Profile")}
-          >
-            <img src={dashboard} alt="dashboard" className="w-4 mr-2" />
-            <p className="">Profile</p>
-          </div>
-          <div
-            className={`flex items-center gap-2 pl-10 w-full hover:bg-gray-300 p-4 cursor-pointer hover:font-semibold ${
-              activeOption === "Billing" ? "bg-gray-300" : ""
-            }`}
-            onClick={() => setActiveOption("Billing")}
-          >
-            <img src={dashboard} alt="dashboard" className="w-4 mr-2" />
-            <p className="">Billing</p>
-          </div>
-          <div
-            className={`flex items-center gap-2 pl-10 w-full hover:bg-gray-300 p-4 cursor-pointer hover:font-semibold ${
-              activeOption === "Email" ? "bg-gray-300" : ""
-            }`}
-            onClick={() => setActiveOption("Email")}
-          >
-            <img src={dashboard} alt="dashboard" className="w-4 mr-2" />
-            <p className="">Email</p>
-          </div>
+          {showSettingsSuboptions && (
+            <>
+              <div
+                className={`flex items-center gap-2 pl-10 w-full hover:bg-gray-300 p-4 cursor-pointer hover:font-semibold ${
+                  activeOption === "Profile" ? "bg-gray-300" : ""
+                }`}
+                onClick={() => setActiveOption("Profile")}
+              >
+                <img src={dashboard} alt="dashboard" className="w-4 mr-2" />
+                <p className="">Profile</p>
+              </div>
+              <div
+                className={`flex items-center gap-2 pl-10 w-full hover:bg-gray-300 p-4 cursor-pointer hover:font-semibold ${
+                  activeOption === "Billing" ? "bg-gray-300" : ""
+                }`}
+                onClick={() => setActiveOption("Billing")}
+              >
+                <img src={dashboard} alt="dashboard" className="w-4 mr-2" />
+                <p className="">Billing</p>
+              </div>
+              <div
+                className={`flex items-center gap-2 pl-10 w-full hover:bg-gray-300 p-4 cursor-pointer hover:font-semibold ${
+                  activeOption === "Email" ? "bg-gray-300" : ""
+                }`}
+                onClick={() => setActiveOption("Email")}
+              >
+                <img src={dashboard} alt="dashboard" className="w-4 mr-2" />
+                <p className="">Email</p>
+              </div>
+            </>
+          )}
           <div
             className={`flex items-center gap-2 w-full hover:bg-gray-300 p-4 cursor-pointer hover:font-semibold pl-4 ${
-              activeOption === "HelpCenter" ? "bg-gray-300" : ""
+              [
+                "HelpCenter",
+                "GettingStarted",
+                "UsingBelo",
+                "SubBilling",
+                "TeamsAccess",
+                "Integration",
+                "TroubleShooting",
+              ].includes(activeOption)
+                ? "bg-gray-200"
+                : ""
             }`}
-            onClick={() => setActiveOption("HelpCenter")}
+            onClick={() => {
+              setActiveOption("HelpCenter");
+              toggleHelpCenterSuboptions();
+            }}
           >
             <img src={dashboard} alt="dashboard" className="w-4 mr-2" />
             <p className="">Help Center</p>
           </div>
-          <div
-            className={`flex items-center gap-2 pl-10 w-full hover:bg-gray-300 p-4 cursor-pointer hover:font-semibold ${
-              activeOption === "GettingStarted" ? "bg-gray-300" : ""
-            }`}
-            onClick={() => setActiveOption("GettingStarted")}
-          >
-            <img src={dashboard} alt="dashboard" className="w-4 mr-2" />
-            <p className="">Getting Started</p>
-          </div>
-          <div
-            className={`flex items-center gap-2 pl-10 w-full hover:bg-gray-300 p-4 cursor-pointer hover:font-semibold ${
-              activeOption === "UsingBelo" ? "bg-gray-300" : ""
-            }`}
-            onClick={() => setActiveOption("UsingBelo")}
-          >
-            <img src={dashboard} alt="dashboard" className="w-4 mr-2" />
-            <p className="">Using Belo</p>
-          </div>
-          <div
-            className={`flex items-center gap-2 pl-10 w-full hover:bg-gray-300 p-4 cursor-pointer hover:font-semibold ${
-              activeOption === "SubBilling" ? "bg-gray-300" : ""
-            }`}
-            onClick={() => setActiveOption("SubBilling")}
-          >
-            <img src={dashboard} alt="dashboard" className="w-4 mr-2" />
-            <p className="">Subscription & Billing</p>
-          </div>
-          <div
-            className={`flex items-center gap-2 pl-10 w-full hover:bg-gray-300 p-4 cursor-pointer hover:font-semibold ${
-              activeOption === "TeamsAccess" ? "bg-gray-300" : ""
-            }`}
-            onClick={() => setActiveOption("TeamsAccess")}
-          >
-            <img src={dashboard} alt="dashboard" className="w-4 mr-2" />
-            <p className="">Teams & Access</p>
-          </div>
-          <div
-            className={`flex items-center gap-2 pl-10 w-full hover:bg-gray-300 p-4 cursor-pointer hover:font-semibold ${
-              activeOption === "Integration" ? "bg-gray-300" : ""
-            }`}
-            onClick={() => setActiveOption("Integration")}
-          >
-            <img src={dashboard} alt="dashboard" className="w-4 mr-2" />
-            <p className="">Integration</p>
-          </div>
-          <div
-            className={`flex items-center gap-2 pl-10 w-full hover:bg-gray-300 p-4 cursor-pointer hover:font-semibold ${
-              activeOption === "TroubleShooting" ? "bg-gray-300" : ""
-            }`}
-            onClick={() => setActiveOption("TroubleShooting")}
-          >
-            <img src={dashboard} alt="dashboard" className="w-4 mr-2" />
-            <p className="">Trouble Shooting</p>
-          </div>
+          {showHelpCenterSuboptions && (
+            <>
+              <div
+                className={`flex items-center gap-2 pl-10 w-full hover:bg-gray-300 p-4 cursor-pointer hover:font-semibold ${
+                  activeOption === "GettingStarted" ? "bg-gray-300" : ""
+                }`}
+                onClick={() => setActiveOption("GettingStarted")}
+              >
+                <img src={dashboard} alt="dashboard" className="w-4 mr-2" />
+                <p className="">Getting Started</p>
+              </div>
+              <div
+                className={`flex items-center gap-2 pl-10 w-full hover:bg-gray-300 p-4 cursor-pointer hover:font-semibold ${
+                  activeOption === "UsingBelo" ? "bg-gray-300" : ""
+                }`}
+                onClick={() => setActiveOption("UsingBelo")}
+              >
+                <img src={dashboard} alt="dashboard" className="w-4 mr-2" />
+                <p className="">Using Belo</p>
+              </div>
+              <div
+                className={`flex items-center gap-2 pl-10 w-full hover:bg-gray-300 p-4 cursor-pointer hover:font-semibold ${
+                  activeOption === "SubBilling" ? "bg-gray-300" : ""
+                }`}
+                onClick={() => setActiveOption("SubBilling")}
+              >
+                <img src={dashboard} alt="dashboard" className="w-4 mr-2" />
+                <p className="">Subscription & Billing</p>
+              </div>
+              <div
+                className={`flex items-center gap-2 pl-10 w-full hover:bg-gray-300 p-4 cursor-pointer hover:font-semibold ${
+                  activeOption === "TeamsAccess" ? "bg-gray-300" : ""
+                }`}
+                onClick={() => setActiveOption("TeamsAccess")}
+              >
+                <img src={dashboard} alt="dashboard" className="w-4 mr-2" />
+                <p className="">Teams & Access</p>
+              </div>
+              <div
+                className={`flex items-center gap-2 pl-10 w-full hover:bg-gray-300 p-4 cursor-pointer hover:font-semibold ${
+                  activeOption === "Integration" ? "bg-gray-300" : ""
+                }`}
+                onClick={() => setActiveOption("Integration")}
+              >
+                <img src={dashboard} alt="dashboard" className="w-4 mr-2" />
+                <p className="">Integration</p>
+              </div>
+              <div
+                className={`flex items-center gap-2 pl-10 w-full hover:bg-gray-300 p-4 cursor-pointer hover:font-semibold ${
+                  activeOption === "TroubleShooting" ? "bg-gray-300" : ""
+                }`}
+                onClick={() => setActiveOption("TroubleShooting")}
+              >
+                <img src={dashboard} alt="dashboard" className="w-4 mr-2" />
+                <p className="">Trouble Shooting</p>
+              </div>
+            </>
+          )}
         </div>
       </div>
       <div className="w-5/6 bg-white">
